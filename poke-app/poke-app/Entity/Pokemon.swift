3de0c575ae09daeb086ea4669be472df15e16aa9
Juan Luis Vázquez Hernández <jvazquez2098@outlook.com>
2025-04-23 14:46:45 -0600

    Fix Poke Cell

--- poke-app/poke-app/Entity/Pokemon.swift
+++ poke-app/poke-app/Entity/Pokemon.swift
@@ -1,42 +1,27 @@
 import Foundation
+import UIKit
 
 struct Pokemon: Codable {
     let id: Int
     let name: String
     let height: Int
     let order: Int
-    let sprites: Sprites
-    let image: String?
-    let description: String?
     let types: [PokemonType]
-}
-
-struct Sprites: Codable {
-    let other: OtherSprites
-}
-
-struct OtherSprites: Codable {
-    let officialArtwork: OfficialArtwork
-    
+    let sprite: String?
+    let description: String?
+    var image: UIImage?
     enum CodingKeys: String, CodingKey {
-        case officialArtwork = "official-artwork"
-    }
-}
-
-struct OfficialArtwork: Codable {
-    let frontDefault: String?
-    
-    enum CodingKeys: String, CodingKey {
-        case frontDefault = "front_default"
+        case id
+        case name
+        case height
+        case order
+        case types
+        case sprite
+        case description
     }
 }
 
 struct PokemonType: Codable {
-    let slot: Int
-    let type: TypeInfo
-}
-
-struct TypeInfo: Codable {
     let name: String
-    let url: String
+    let id: Int
 }
